2a02327d8779dd7f39acc0e8dfc17a2c774f37c6
Gyomdyung <54757435+sung-woo-jang@users.noreply.github.com>
2023-04-13 21:18:47 +0900

    useQuery 맛보기 - 2023년 4월 13일 목요일 21시 18분 46초 KST

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,41 +1,73 @@
+import { Fragment } from 'react';
+import { useMutation, useQuery } from 'react-query';
+
 async function fetchComments(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
-  );
-  return response.json();
+    const response = await fetch(
+        `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
+    );
+    return response.json();
 }
 
 async function deletePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "DELETE" }
-  );
-  return response.json();
+    const response = await fetch(
+        `https://jsonplaceholder.typicode.com/postId/${postId}`,
+        { method: 'DELETE' }
+    );
+    return response.json();
 }
 
 async function updatePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
-  );
-  return response.json();
+    const response = await fetch(
+        `https://jsonplaceholder.typicode.com/postId/${postId}`,
+        { method: 'PATCH', data: { title: 'REACT QUERY FOREVER!!!!' } }
+    );
+    return response.json();
 }
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+    // replace with useQuery
 
-  return (
-    <>
-      <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
-      <p>{post.body}</p>
-      <h4>Comments</h4>
-      {data.map((comment) => (
-        <li key={comment.id}>
-          {comment.email}: {comment.body}
-        </li>
-      ))}
-    </>
-  );
+    const { data, isError, isLoading } = useQuery(['comments', post.id], () =>
+        fetchComments(post.id)
+    );
+
+    const deleteMutation = useMutation((postId) => deletePost(postId));
+
+    const updateMutaion = useMutation((postId) => updatePost(postId));
+
+    if (isLoading) return <h3>Loding...</h3>;
+    if (isError) return <h3>Error...</h3>;
+
+    return (
+        <Fragment>
+            <h3 style={{ color: 'blue' }}>{post.title}</h3>
+            <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+            <button onClick={() => updateMutaion.mutate(post.id)}>Update title</button>
+            {deleteMutation.isError && (
+                <p style={{ color: 'red' }}>Error deleting the post</p>
+            )}
+            {deleteMutation.isLoading && (
+                <p style={{ color: 'purple' }}>Deleting the post</p>
+            )}
+            {deleteMutation.isSuccess && (
+                <p style={{ color: 'green' }}>Post has (not) been deleted</p>
+            )}
+            {updateMutaion.isError && (
+                <p style={{ color: 'red' }}>Error updating the post</p>
+            )}
+            {updateMutaion.isLoading && (
+                <p style={{ color: 'purple' }}>Updating the post</p>
+            )}
+            {updateMutaion.isSuccess && (
+                <p style={{ color: 'green' }}>Post has (not) been updated</p>
+            )}
+            <p>{post.body}</p>
+            <h4>Comments</h4>
+            {data.map((comment) => (
+                <li key={comment.id}>
+                    {comment.email}: {comment.body}
+                </li>
+            ))}
+        </Fragment>
+    );
 }
--- base-infinite-swapi/src/App.jsx
+++ base-infinite-swapi/src/App.jsx
@@ -1,15 +1,15 @@
-import "./App.css";
-import { InfinitePeople } from "./people/InfinitePeople";
-import { InfiniteSpecies } from "./species/InfiniteSpecies";
+import './App.css';
+import { InfinitePeople } from './people/InfinitePeople';
+import { InfiniteSpecies } from './species/InfiniteSpecies';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Infinite SWAPI</h1>
-      <InfinitePeople />
-      {/* <InfiniteSpecies /> */}
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Infinite SWAPI</h1>
+            <InfinitePeople />
+            {/* <InfiniteSpecies /> */}
+        </div>
+    );
 }
 
 export default App;
