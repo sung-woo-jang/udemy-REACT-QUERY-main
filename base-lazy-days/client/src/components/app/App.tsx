190b27ca7298a07aa52f6d7ba798995105cba1b1
gyomdyung <54757435+sung-woo-jang@users.noreply.github.com>
2023-04-18 06:34:00 +0900

    1

--- base-lazy-days/client/src/components/app/App.tsx
+++ base-lazy-days/client/src/components/app/App.tsx
@@ -1,13 +1,13 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import { ReactElement } from 'react';
-import { QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
+import { ChakraProvider } from "@chakra-ui/react";
+import { ReactElement } from "react";
+import { QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
-import { queryClient } from '../../react-query/queryClient';
-import { theme } from '../../theme';
-import { Loading } from './Loading';
-import { Navbar } from './Navbar';
-import { Routes } from './Routes';
+import { queryClient } from "../../react-query/queryClient";
+import { theme } from "../../theme";
+import { Loading } from "./Loading";
+import { Navbar } from "./Navbar";
+import { Routes } from "./Routes";
 
 export function App(): ReactElement {
   return (
