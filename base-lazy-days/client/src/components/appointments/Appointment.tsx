3caa5229a823cd4a9a0506c01e0f310061850033
gyomdyung <54757435+sung-woo-jang@users.noreply.github.com>
2023-04-20 21:52:36 +0900

    Thu Apr 20 21:52:36     2023

--- base-lazy-days/client/src/components/appointments/Appointment.tsx
+++ base-lazy-days/client/src/components/appointments/Appointment.tsx
@@ -9,63 +9,56 @@ import { useReserveAppointment } from './hooks/useReserveAppointment';
 import { appointmentInPast, getAppointmentColor } from './utils';
 
 // determine whether this appointment can be reserved / un-reserved by logged-in user
-function isClickable(
-  user: User | null,
-  appointmentData: AppointmentType,
-): boolean {
-  return !!(
-    user?.id &&
-    (!appointmentData.userId || appointmentData.userId === user?.id) &&
-    !appointmentInPast(appointmentData)
-  );
+function isClickable(user: User | null, appointmentData: AppointmentType): boolean {
+    return !!(
+        user?.id &&
+        (!appointmentData.userId || appointmentData.userId === user?.id) &&
+        !appointmentInPast(appointmentData)
+    );
 }
 
 interface AppointmentProps {
-  appointmentData: AppointmentType;
+    appointmentData: AppointmentType;
 }
 
-export function Appointment({
-  appointmentData,
-}: AppointmentProps): ReactElement {
-  const { user } = useUser();
-  const reserveAppointment = useReserveAppointment();
-  const [textColor, bgColor] = getAppointmentColor(appointmentData, user?.id);
+export function Appointment({ appointmentData }: AppointmentProps): ReactElement {
+    const { user } = useUser();
+    const reserveAppointment = useReserveAppointment();
+    const [textColor, bgColor] = getAppointmentColor(appointmentData, user?.id);
 
-  const clickable = isClickable(user, appointmentData);
-  let onAppointmentClick: undefined | (() => void);
-  let hoverCss = {};
+    const clickable = isClickable(user, appointmentData);
+    let onAppointmentClick: undefined | (() => void);
+    let hoverCss = {};
 
-  // turn the lozenge into a button if it's clickable
-  if (clickable) {
-    onAppointmentClick = user
-      ? () => reserveAppointment(appointmentData)
-      : undefined;
-    hoverCss = {
-      transform: 'translateY(-1px)',
-      boxShadow: 'md',
-      cursor: 'pointer',
-    };
-  }
+    // turn the lozenge into a button if it's clickable
+    if (clickable) {
+        onAppointmentClick = user ? () => reserveAppointment(appointmentData) : undefined;
+        hoverCss = {
+            transform: 'translateY(-1px)',
+            boxShadow: 'md',
+            cursor: 'pointer',
+        };
+    }
 
-  const appointmentHour = dayjs(appointmentData.dateTime).format('h a');
-  return (
-    <Box
-      borderRadius="lg"
-      px={2}
-      bgColor={bgColor}
-      color={textColor}
-      as={clickable ? 'button' : 'div'}
-      onClick={onAppointmentClick}
-      _hover={hoverCss}
-    >
-      <HStack justify="space-between">
-        <Text as="span" fontSize="xs">
-          {appointmentHour}
-        </Text>
-        <Text as="span" fontSize="xs">
-          {appointmentData.treatmentName}
-        </Text>
-      </HStack>
-    </Box>
-  );
+    const appointmentHour = dayjs(appointmentData.dateTime).format('h a');
+    return (
+        <Box
+            borderRadius="lg"
+            px={2}
+            bgColor={bgColor}
+            color={textColor}
+            as={clickable ? 'button' : 'div'}
+            onClick={onAppointmentClick}
+            _hover={hoverCss}
+        >
+            <HStack justify="space-between">
+                <Text as="span" fontSize="xs">
+                    {appointmentHour}
+                </Text>
+                <Text as="span" fontSize="xs">
+                    {appointmentData.treatmentName}
+                </Text>
+            </HStack>
+        </Box>
+    );
 }
